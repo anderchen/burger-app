8cf9cb7c56837fb62293d980eac8166a4917f053
Anderson Chen <ander.chen00@gmail.com>
2019-08-21 09:16:43 -0300

    Add prices in the build controls

--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -49,7 +49,7 @@ class BurgerBuilder extends Component {
 
     const priceDeduction = INGREDIENT_PRICES[type];
     const oldPrice = this.state.totalPrice;
-    const newPrice = oldPrice + priceDeduction;
+    const newPrice = oldPrice - priceDeduction;
 
     this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
   }
@@ -69,6 +69,7 @@ class BurgerBuilder extends Component {
           ingredientAdded={this.addIngredientHandler}
           ingredientRemoved={this.removeIngredientHandler}
           disabled={disabledInfo}
+          price={this.state.totalPrice}
         />
       </Aux>
     );
