5ea6b88974a1dcd6f384290fd74019d56c4f62b5
Anderson Chen <ander.chen00@gmail.com>
2019-08-22 11:42:52 -0300

    Add buttons to the modal

--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -80,6 +80,12 @@ class BurgerBuilder extends Component {
   purchaseCancelHandler = () => {
     this.setState({purchasing: false});
   }
+
+  purchaseContinueHandler = () => {
+    alert("You Continue!");
+  }
+
+
   render () {
     const disabledInfo ={
       ...this.state.ingredients
@@ -91,7 +97,11 @@ class BurgerBuilder extends Component {
     return (
       <Aux>
         <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-          <OrderSummary ingredients={this.state.ingredients}/>
+          <OrderSummary ingredients={this.state.ingredients}
+                        purchaseCancelled={this.purchaseCancelHandler}
+                        purchaseContinued={this.purchaseContinueHandler}
+                        price={this.state.totalPrice}
+          />
         </Modal>
         <Burger ingredients={this.state.ingredients}/>
         <BuildControls 
