47e235939ab2858e19e09536f84f84400f41733f
Anderson Chen <ander.chen00@gmail.com>
2019-08-23 11:47:27 -0300

    Handle get and post from the firebase api

--- src/hoc/withErrorHandler/withErrorHandler.jsx
+++ src/hoc/withErrorHandler/withErrorHandler.jsx
@@ -8,8 +8,8 @@ const withErrorHandler = (WrappedComponent, axios) => {
     state = {
       error: null
     }
-
-    componentDidMount () {
+    //componentWillMount is deprecated. Use constructor
+    componentWillMount () {
       axios.interceptors.request.use(req => {
         this.setState({error: null});
         return req;
