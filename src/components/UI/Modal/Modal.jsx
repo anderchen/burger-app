cc0f9cfa5051a0cf88c3b1ff37e262d3aae8dde7
Anderson Chen <ander.chen00@gmail.com>
2019-08-23 10:53:39 -0300

    Handle errors with a Higher Order Component

--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -6,6 +6,7 @@ import Backdrop from '../Backdrop/Backdrop';
 
 class Modal extends Component {
   shouldComponentUpdate(nextProps, nextState) {
+    // console.log(this.props.children)
     return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
   }
 
