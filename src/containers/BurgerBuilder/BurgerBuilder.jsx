b6d23cb55acaba350c633d8bdeeda90e101df705
Anderson Chen <ander.chen00@gmail.com>
2019-08-22 23:10:01 -0300

    Send data to firebase when order is completed in the modal

--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -7,6 +7,8 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
 
+import axios from '../../axios-orders';
+
 const INGREDIENT_PRICES = {
   salad: 0.5,
   cheese: 0.4,
@@ -82,7 +84,29 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    alert("You Continue!");
+    // alert("You Continue!");
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Anderson Chen',
+        address: {
+          street: 'Test Street 123',
+          zipcode: '123456',
+          country: 'Brazil'
+        },
+        email: 'test@test.com',
+      },
+      deliveryMethod: 'Flash style fast'
+    }
+    axios.post('/orders.json', order)
+          .then(response => {
+            console.log(response);
+          })
+          .catch(error => {
+            console.log(error);
+          });
+
   }
 
 
